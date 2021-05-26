9febdee20d3093147212f399a43fdb780837bcb3
Alexandre Morey <alexander006_9@hotmail.com>
2021-05-26 15:40:02 +0100

    added the create part for the API

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+
   def index
     @restaurants = policy_scope(Restaurant)
   end
@@ -6,5 +8,37 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
   def show
     @restaurant = Restaurant.find(params[:id])
     authorize @restaurant
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant
+    if @restaurant.update(restaurant_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+    authorize @restaurant
+    if @restaurant.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -8,4 +8,12 @@ class RestaurantPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def update?
+    record.user == user
+  end
+
+  def create?
+    !user.nil?
+  end
 end
